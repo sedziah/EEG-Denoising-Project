f740c66eee658b7224d0c6a3bd740f31c9439a15
Solomon Edziah <solomon@Harshas-Mac-Studio.local>
2024-11-13 16:37:49 +0100

    Add project parameters

---
+++ code/parameters.py
@@ -0,0 +1,6 @@
+# Signal Generation Parameters
+
+type_of_stimulus = "Pattern Reversal VEP"
+simulation_frequency = 7.5 #Hz
+sampling_frequency = 1000 #Hz ISCEV recommendation
+analysis_time = 250 # ms ISCEV recommendation
